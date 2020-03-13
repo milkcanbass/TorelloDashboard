f9e6b15a9e4440ed60a01645c41d37683076b505
milkcan <milkcanbass@gmail.com>
2020-03-12 20:07:45 -0400

    update

---
+++ client/build/precache-manifest.661aa91759df2027d8cbeadabcffa6e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99bd6501171b7eb7d426860352064a5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46b4a6415973edc449f",
+    "url": "/static/css/2.f477f4b5.chunk.css"
+  },
+  {
+    "revision": "1dddb13281c626e615fd",
+    "url": "/static/css/main.a7161b50.chunk.css"
+  },
+  {
+    "revision": "a46b4a6415973edc449f",
+    "url": "/static/js/2.d46fec01.chunk.js"
+  },
+  {
+    "revision": "fc017ea0f477442f50559a9569b362cd",
+    "url": "/static/js/2.d46fec01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dddb13281c626e615fd",
+    "url": "/static/js/main.06be515c.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "a3921ba30e7c1101342abd534eb354c9",
+    "url": "/static/media/login.a3921ba3.png"
+  }
+]);
