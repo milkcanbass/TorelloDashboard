444c0b86fef659f2600e759001258414f74c6848
milkcan <milkcanbass@gmail.com>
2020-03-14 19:35:13 -0400

    upload

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ app.use(function(err, req, res, next) {
 });
 
 if (process.env.NODE_ENV === "production") {
+  //set static folder
   app.use(express.static("client/build"));
-
-  app.get("*", (request, response) => {
-    response.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
 }
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
