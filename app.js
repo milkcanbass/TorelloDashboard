e983dca82777e5149f361fe5137d7593197a300e
milkcan <milkcanbass@gmail.com>
2020-03-14 18:55:22 -0400

    update command

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const userRouter = require("./routes/user");
 const dashboardRouter = require("./routes/dashboard");
 const calendarRouter = require("./routes/calendar");
 const fileRouter = require("./routes/file");
+const path = require("path");
 
 const { json, urlencoded } = express;
 
@@ -40,5 +41,12 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.json({ error: err });
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", index.html));
+  });
+}
 
 module.exports = app;
