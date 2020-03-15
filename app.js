5c5038ee083580a97fe2e26b0ec4c19b63e018d3
milkcan <milkcanbass@gmail.com>
2020-03-15 14:23:39 -0400

    update

--- app.js
+++ app.js
@@ -29,20 +29,20 @@ app.use("/calendar", calendarRouter);
 app.use("/file", fileRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.json({ error: err });
+// });
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build")); // serve the static react app
