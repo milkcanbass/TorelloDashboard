c720e62bb16eff2c0c79539570670b934a07576f
milkcan <milkcanbass@gmail.com>
2020-03-15 21:31:42 -0400

    change routes

--- client/src/App.js
+++ client/src/App.js
@@ -16,14 +16,15 @@ function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <PrivateRoute path='/dashboards/:dashboardId' component={DashBoard} />
+        <PrivateRoute exact path='/dashboards/:dashboardId' component={DashBoard} />
         <Route exact path='/' component={SignIn} />
-        <Route path='/signup' component={SignUp} />
+        <Route exact path='/signup' component={SignUp} />
         <PrivateRoute
+          exact
           path='/(dashboards|calendar)/:dashboardId/columns/:columnId/tasks/:taskId'
           render={props => <CardModal {...props} />}
         />
-        <Route path='/calendar/:dashboardId' component={Calendar} />
+        <Route exact path='/calendar/:dashboardId' component={Calendar} />
       </BrowserRouter>
     </MuiThemeProvider>
   );
